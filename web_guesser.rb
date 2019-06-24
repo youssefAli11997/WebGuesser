afcef0f8824e3d59ef6d279f9132a115d5fdb2c0
Youssef Ali <youssefali6212@gmail.com>
2019-06-24 16:45:12 +0200

    I4.1: Changing background color according to user's guessing

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,6 +3,7 @@ require 'sinatra/reloader'
 
 $SECRET_NUMBER = rand(100)
 $correct = false
+$bg_color = "white"
 
 def check_guess(guess)
 	begin
@@ -11,18 +12,25 @@ def check_guess(guess)
 			if $SECRET_NUMBER == guess
 				message = "Correct"
 				$correct = true
+				$bg_color = "'lime'"
 			elsif $SECRET_NUMBER - guess > 30 && $SECRET_NUMBER - guess < 50
 				message = "Too Low"
+				$bg_color = "'lightcoral'"
 			elsif $SECRET_NUMBER - guess >= 50
 				message = "Way too Low"
+				$bg_color = "'red'"
 			elsif guess - $SECRET_NUMBER > 30 && guess - $SECRET_NUMBER < 50
 				message = "Too High"
+				$bg_color = "'lightcoral'"
 			elsif guess - $SECRET_NUMBER >= 50
 				message = "Way too High"
+				$bg_color = "'red'"
 			elsif $SECRET_NUMBER - guess <= 30 && $SECRET_NUMBER - guess >= 0
 				message = "Increase your guess!"
+				$bg_color = "'peachpuff'"
 			else
 				message = "Decrease your guess!"
+				$bg_color = "'peachpuff'"
 			end
 		end
 	rescue Exception => ex
@@ -34,5 +42,8 @@ end
 get '/'  do
 	guess = params['guess']
 	message = check_guess(guess)
-	erb :index, :locals => {:number => $SECRET_NUMBER, :message => message, :correct => $correct}
+	erb :index, :locals => {:number => $SECRET_NUMBER,
+							:message => message, 
+							:correct => $correct,
+							:bg_color => $bg_color}
 end
